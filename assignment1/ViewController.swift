6a61fa474ac88efcb49949aae208f47d02908e82
Mohammad Naqvi <mrn0035@auburn.edu>
2026-05-23 20:35:21 -0400

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Mohammad Raza Naqvi on 5/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
